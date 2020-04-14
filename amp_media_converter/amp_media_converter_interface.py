1df8ca7347dd8b41427742950cd5133d074faa19
Mike Jones <michaeljones401@gmail.com>
2020-04-14 18:58:20 +0100

    adding comments

--- amp_media_converter/amp_media_converter_interface.py
+++ amp_media_converter/amp_media_converter_interface.py
@@ -1,48 +1,48 @@
 class AMP_Media_Converter_Interface:
-    ''' TEXT
+    ''' Abstract Class for media converter classes
+
+        Any class that converts media should inherit from this class
+        and all method within this class should be implemented
+        within these sub-classes
     '''
 
     def __init__(self):
         pass
 
     def get_directory_path(self):
-        ''' TEXT
-        '''
-
-        raise NotImplementedError("Subclass must implement this abstract method")
-
-    def search_files(self):
-        ''' TEXT
+        ''' Method to prompt the user for a directory path
         '''
 
         raise NotImplementedError("Subclass must implement this abstract method")
 
     def write_file(self):
-        ''' TEXT
+        ''' Method to allow a user to write a file
         '''
 
         raise NotImplementedError("Subclass must implement this abstract method")
 
     def delete_file(self):
-        ''' TEXT
+        ''' Method to allow a user to delete a file
         '''
 
         raise NotImplementedError("Subclass must implement this abstract method")
 
     def move_file(self):
-        ''' TEXT
+        ''' Method to allow a user to move a file
         '''
 
         raise NotImplementedError("Subclass must implement this abstract method")
 
     def convert(self):
-        ''' TEXT
+        ''' Method to convert media
+            it should accept a list parameter - file_extensions
+            which the class should search for an convert 
         '''
 
         raise NotImplementedError("Subclass must implement this abstract method")
 
     def create_directory(self):
-        ''' TEXT
+        ''' Method to allow a user to create a directory
         '''
 
         raise NotImplementedError("Subclass must implement this abstract method")
